5ae81c3a8b4f2ea510edc3ff5f874fcdc0cc181b
Dunmi <dominic.anyanna@gmail.com>
2020-02-17 18:31:28 +0100

    added lawyer and client menu

--- __manifest__.py
+++ __manifest__.py
@@ -20,12 +20,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'mail', 'contacts', 'account'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'data/sequence.xml',
         'views/views.xml',
+        'views/actions.xml',
+        'views/menu.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- models/models.py
+++ models/models.py
@@ -1,6 +1,91 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+
+
+class Matter(models.Model):
+    _name = 'matter.24law'
+    _rec_name = 'name'
+    _description = 'New Description'
+
+    name = fields.Char()
+    matter_no = fields.Char(string="Matter Number",
+                            default=lambda self: _('New'), requires=False, readonly=True,)
+    client_id = fields.Many2one(comodel_name="res.partner", string="Client", required=False, )
+    open_date = fields.Date(string="Openning Date", required=False, )
+    type_matter = fields.Selection(string="Type", selection=[('civil', 'Civil'), ('criminal', 'Criminal'), ], required=False, )
+    category_matter = fields.Selection(string="Category", selection=[('first', 'First'), ('second', 'Second')])
+    lawyer = fields.Many2one(comodel_name="res.user", string="Lawyer Assigned")
+
+    @api.model
+    def create(self, vals):
+        if vals.get('matter_no', _('New')) == _('New'):
+            vals['matter_no'] = self.env['ir.sequence'].next_by_code('increment_matter') or _('New')
+        result = super(Matter, self).create(vals)
+        return result
+
+class Evidence(models.Model):
+    _name = 'evidence.24law'
+    _rec_name = 'name'
+    _description = 'New Description'
+
+    name = fields.Char()
+
+class Actions(models.Model):
+    _name = 'actions.24law'
+    _rec_name = 'name'
+    _description = 'New Description'
+
+    name = fields.Char()
+class Courts(models.Model):
+    _name = 'courts.24law'
+    _rec_name = 'name'
+    _description = 'New Description'
+
+    name = fields.Char()
+
+class Judge(models.Model):
+    _name = 'judge.24law'
+    _rec_name = 'name'
+    _description = 'New Description'
+
+    name = fields.Char()
+
+
+class Documents(models.Model):
+    _name = 'documents.24law'
+    _rec_name = 'name'
+    _description = 'New Description'
+
+    name = fields.Char()
+
+
+class Expenses(models.Model):
+    _name = 'expenses.24law'
+    _rec_name = 'name'
+    _description = 'New Description'
+
+    name = fields.Char()
+
+
+class Task(models.Model):
+    _name = 'task.24law'
+    _rec_name = 'name'
+    _description = 'New Description'
+
+    name = fields.Char()
+
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    is_lawyer = fields.Boolean(string="Lawyer", default=True)
+
+
+
+
+
+
 
 # class 24law(models.Model):
 #     _name = '24law.24law'
